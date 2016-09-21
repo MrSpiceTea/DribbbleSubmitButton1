592c5a5dc6829b6838a4d30bdd2791d283cf3e08
xzt493136304 <493136304@qq.com>
2016-09-21 18:03:04 +0800

    INIT

---
+++ DribbbleSubmitButton1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DribbbleSubmitButton1
+//
+//  Created by Mr.Xie on 16/9/21.
+//  Copyright © 2016年 XZT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DribbbleSubmitButton1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DribbbleSubmitButton1
+//
+//  Created by Mr.Xie on 16/9/21.
+//  Copyright © 2016年 XZT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
